a6f47339fa195772d16ead6ed02017961bfec51a
aybaman <aybekmamasaliev6@gmail.com>
2024-07-07 01:06:46 +0600

    feat: add header search component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  isSearchBar:boolean = true
+
+
+  constructor(){
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  showSearchInput(){
+    this.isSearchBar = !this.isSearchBar
+  }
 }
